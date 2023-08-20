60cc5eb886d144348bd2ba5775c52851a7b9fa9c
Vivianne Le <viviannele2001@gmail.com>
2023-08-20 01:33:24 -0700

    create script for player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if(change != Vector3.zero) {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position + change.normalized * speed * Time.deltaTime);
+    }
+}
